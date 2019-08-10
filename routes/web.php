93d1928495d11dd11d9f60bc05ed7d0bde04af16
IronySoft <ironyoft.ltd@gmail.com>
2019-08-10 11:02:10 +0600

    Chnage Pass. Controller Added and Routed

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/change-password', 'Auth\ChangePasswordController@index')->name('password.change');
