60123e4304559cab09ca63fa2f7f02f7ac70ac99
IronySoft <ironyoft.ltd@gmail.com>
2019-08-10 12:44:49 +0600

    ChangeView Type Change

--- resources/views/auth/passwords/change.blade.php
+++ resources/views/auth/passwords/change.blade.php
@@ -22,7 +22,7 @@
                                        class="col-md-4 col-form-label text-md-right">{{ __('Old Password') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="old-password" type="text"
+                                    <input id="old-password" type="password"
                                            class="form-control @error('oldPassword') is-invalid @enderror"
                                            name="oldPassword" value="{{ old('oldPassword') }}" required
                                            autocomplete="oldPassword" autofocus>
