e63d3d657e0709f3b3739379c15e35e571f20719
IronySoft <ironyoft.ltd@gmail.com>
2019-08-10 11:27:51 +0600

    Change Password View Ready

--- app/Http/Controllers/Auth/ChangePasswordController.php
+++ app/Http/Controllers/Auth/ChangePasswordController.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\Controller;
 
 class ChangePasswordController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('auth.passwords.change');
+    }
 }
